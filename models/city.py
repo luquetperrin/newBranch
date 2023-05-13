dbeb78807ba4fe608be85a85f98b95de2a52be41
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-05-13 08:37:07 +0100

    task-9 without writing unittests

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines City class."""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Inherits from BaseModel."""
+    state_id = ''
+    name = ''
